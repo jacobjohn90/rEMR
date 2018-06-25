4eaca81a29d97ee65d4915db9aee5346826c577f
jacobjohn90 <jacobjohn90@gmail.com>
2018-06-25 16:56:51 -0400

    both get routes and post route complete and tested on postman for doctor model

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { DoctorModel } = require('../db/schema')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  DoctorModel.find().then((doctors) => {
+    res.json({ doctors })
+  })
+    .catch((err) =>
+      console.log('Error getting to /doctors on Express. Error is: ' + err))
 });
+
+router.get('/:id', (req, res) => {
+  DoctorModel.findById(req.params.id).then((doctor) => {
+    res.send({ doctor })
+  })
+})
+
+router.post('/', (req, res) => {
+  const newDoctor = new DoctorModel(req.body)
+  newDoctor.save().then((doctor)=> {
+    res.send(doctor)
+  })
+})
 
 module.exports = router;
